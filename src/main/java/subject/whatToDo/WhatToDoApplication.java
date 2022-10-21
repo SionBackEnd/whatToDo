8504ee45c737086ad78649bea4e71b7990c2e727
시온백 <tldhs05222@naver.com>
2022-10-21 14:19:06 +0900

    초기 세팅 연동

---
+++ src/main/java/subject/whatToDo/WhatToDoApplication.java
@@ -0,0 +1,13 @@
+package subject.whatToDo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatToDoApplication.class, args);
+	}
+
+}
