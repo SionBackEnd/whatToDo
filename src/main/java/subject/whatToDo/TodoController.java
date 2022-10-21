132bda428e01f26394544bd28c5fdab4c38511f1
시온백 <tldhs05222@naver.com>
2022-10-21 14:25:22 +0900

    기능 구현

---
+++ src/main/java/subject/whatToDo/TodoController.java
@@ -0,0 +1,15 @@
+package subject.whatToDo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoController {
+
+    @GetMapping
+    public String helloWorld() {
+        return "To-do Application !";
+    }
+}
